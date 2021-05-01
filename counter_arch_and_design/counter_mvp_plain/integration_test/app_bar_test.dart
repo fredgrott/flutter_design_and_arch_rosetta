49d5911d7a0ab13d79391160e1f21a672328a777
Fred Grott <fred.grott@gmail.com>
2021-05-01 06:49:05 -0500

    counter mvp plain first pass
    counter mvp plain first pass

---
+++ counter_arch_and_design/counter_mvp_plain/integration_test/app_bar_test.dart
@@ -0,0 +1,38 @@
+// Copyright 2021 Fredrick Allan Grott. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+
+import 'package:counter_mvp_plain/app/shared/app_globals.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+
+import 'app_bar_widget_test_support.dart';
+
+void main() {
+  // ignore: no-empty-block
+  setUpAll(() async {});
+
+  setUp(() async {
+    // ignore: unused_local_variable
+    final WidgetsBinding binding =
+        IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+  });
+
+  // ignore: no-empty-block
+  tearDownAll(() async {});
+
+  // ignore: no-empty-block
+  tearDown(() async {
+    // Code that clears caches can go here
+  });
+
+  group('appbar test', () {
+    testWidgets('AppBar Title is $myAppTitle', harness((given, when, then) async {
+    await given.myAppExists();
+    await when.appBarFound();
+    await then.appBarTitleIs();
+  }));
+  });
+}
