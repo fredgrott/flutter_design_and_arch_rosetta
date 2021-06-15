783cb66d1ff174e00767978a2f3e2c500a58716f
Fred Grott <fred.grott@gmail.com>
2021-06-15 07:58:19 -0500

    added workflows demos
    added workflows demos

---
+++ testing/test_example/android/app/src/main/kotlin/io/github/fredgrott/test_example/MainActivity.kt
@@ -0,0 +1,6 @@
+package io.github.fredgrott.test_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ workarounds/android_gradle_perf/android/app/src/main/kotlin/io/github/fredgrott/android_gradle_perf/MainActivity.kt
@@ -0,0 +1,6 @@
+package io.github.fredgrott.android_gradle_perf
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
